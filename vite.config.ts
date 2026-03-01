f377c51f4172a49a33bc957bdc673b47f22c1b97
Kip Price <4605379+kipprice@users.noreply.github.com>
2026-02-28 21:24:26 -0500

    get ready to publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vitest/config";
+import { resolve } from "path";
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "queryable",
+      formats: ["es", "cjs"],
+      fileName: (format) => `queryable.${format === "es" ? "mjs" : "cjs"}`,
+    },
+  },
   test: {
     globals: true,
     environment: "node",
